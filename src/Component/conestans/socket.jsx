db1c79742f52f2954de7e7cc67e73eee3a51b991
AhmedSdek <93591832+AhmedSdek@users.noreply.github.com>
2025-01-18 18:53:10 +0200

    h

--- src/Component/conestans/socket.jsx
+++ src/Component/conestans/socket.jsx
@@ -1,5 +1,8 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://facebook-backend-production-110d.up.railway.app'); // تعديل الرابط حسب خادمك
-
+// const socket = io('https://facebook-backend-production-110d.up.railway.app'); // تعديل الرابط حسب خادمك
+const socket = io(`https://facebook-backend-production-110d.up.railway.app`, {
+    transports: ["websocket", "polling"], // دعم النقل عبر Polling وWebSocket
+    withCredentials: true
+});
 export default socket;
