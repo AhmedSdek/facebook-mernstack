33d0a57a04a867e511cec6674560ff7af0382f2f
AhmedSdek <93591832+AhmedSdek@users.noreply.github.com>
2025-01-19 20:22:36 +0200

    ا

--- src/Component/conestans/baseurl.jsx
+++ src/Component/conestans/baseurl.jsx
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'https://facebook-backend-production-110d.up.railway.app' 
-export const BASE_URL = ' http://localhost:3000' 
+export const BASE_URL = 'https://facebook-backend-production-110d.up.railway.app'
+// export const BASE_URL = ' http://localhost:3000' 
--- src/Component/conestans/socket.jsx
+++ src/Component/conestans/socket.jsx
@@ -1,10 +1,10 @@
 import { io } from 'socket.io-client';
 
 // const socket = io('https://facebook-backend-production-110d.up.railway.app'); // تعديل الرابط حسب خادمك
-// const socket = io(`https://facebook-backend-production-110d.up.railway.app`, {
-//     transports: ["websocket", "polling"], // دعم النقل عبر Polling وWebSocket
-//     withCredentials: true
-// });
+const socket = io(`https://facebook-backend-production-110d.up.railway.app`, {
+    transports: ["websocket", "polling"], // دعم النقل عبر Polling وWebSocket
+    withCredentials: true
+});
 
-const socket = io(`http://localhost:3000`);
+// const socket = io(`http://localhost:3000`);
 export default socket;
